39a2019304252401fff4afa8e004852af1dba4b8
Zy-on <rodriguessilvadrs@gmail.com>
2021-04-23 12:22:43 -0300

    plants notifications

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -75,6 +75,10 @@ export function PlantSave() {
   }
 
   return (
+   <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.container}
+   >
     <View style={styles.container}>
       <View style={styles.plantInfo}>
         <SvgFromUri uri={plant.photo} height={150} width={150} />
@@ -121,6 +125,7 @@ export function PlantSave() {
         <Button title="Cadastrar planta" onPress={handleSave} />
       </View>
     </View>
+   </ScrollView>
   )
 }
 
